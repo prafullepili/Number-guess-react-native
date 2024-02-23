91451c749bdd193365f750f67f66778696de7ac3
Prafull Epili <prafullepili@gmail.com>
2024-02-23 22:06:10 +0530

    added checks for user lie

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Alert } from "react-native";
 import NumberContainer from "../components/game/NumberContainer";
 import Title from "../components/ui/Title";
 import { useState } from "react";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -11,16 +12,48 @@ function generateRandomBetween(min, max, exclude) {
     return rndNum;
   }
 }
-
+let minBoundary = 1;
+let maxBoundary = 100;
 export default function GameScreen({ userNumber }) {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  }
   return (
     <View style={styles.screen}>
       <Title>Oppenent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
+        <View>
+          <PrimaryButton onPress={() => nextGuessHandler("lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler("greater")}>
+            +
+          </PrimaryButton>
+        </View>
       </View>
       <View>
         <Text>LOG ROUNDS</Text>
