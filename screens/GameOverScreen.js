487d80d3c35c43f3cc6aca0108dd339470b9cfb9
Prafull Epili <prafullepili@gmail.com>
2024-02-24 15:02:50 +0530

    change margin

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,7 @@
 import { View, Image, StyleSheet, Text } from "react-native";
 import Title from "../components/ui/Title";
 import colors from "../constants/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 export default function GameOverScreen() {
   return (
@@ -9,7 +10,11 @@ export default function GameOverScreen() {
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={require("../assets/images/success.png")} />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to guess the number{" "}
+        <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -33,5 +38,15 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
+  },
+  summaryText: {
+    fontFamily: "opne-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: colors.primary500,
   },
 });
